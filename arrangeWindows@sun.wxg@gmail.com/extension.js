20a427e8cd5e1eccaee65e24149e32be47ba70b1
sunwxg <sun.wxg@gmail.com>
2019-01-18 10:14:40 +0800

    updating

--- arrangeWindows@sun.wxg@gmail.com/extension.js
+++ arrangeWindows@sun.wxg@gmail.com/extension.js
@@ -23,16 +23,21 @@ const Convenience = Me.imports.convenience;
 const CASCADE_WIDTH = 30;
 const CASCADE_HEIGHT = 30;
 
+const ARRANGEWINDOWS_SCHEMA = 'org.gnome.shell.extensions.arrangeWindows';
+const ALL_MONITOR = 'all-monitors';
+
 class ArrangeMenu extends PanelMenu.Button {
     constructor() {
         super(0.0, _("Arrange Windows"));
+
+        this._gsettings = Convenience.getSettings(ARRANGEWINDOWS_SCHEMA);
+
+        this._allMonitor = this._gsettings.get_boolean(ALL_MONITOR);
 
         let icon = new St.Icon({ icon_name: 'arrange-windows',
                                  style_class: 'system-status-icon' });
         this.actor.add_child(icon);
 
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        //this.menu.addAction(_("Tile"), () => this.tileWindow());
         this.menu.addAction(_("Cascade"),
                             () => this.cascadeWindow(),
                             "cascade-windows-symbolic");
@@ -40,6 +45,10 @@ class ArrangeMenu extends PanelMenu.Button {
         this.menu.addAction(_("Side by side"),
                             () => this.sideBySideWindow(),
                             "sidebyside-windows-symbolic");
+
+        this.menu.addAction(_("Stack"),
+                            () => this.stackWindow(),
+                            "stack-windows-symbolic");
 
         this.menu.addAction(_("Maximize"),
                             () => this.maximizeWindow(Meta.MaximizeFlags.BOTH),
@@ -57,31 +66,13 @@ class ArrangeMenu extends PanelMenu.Button {
                             () => this.restoringWindow(),
                             "restoring-window-symbolic");
 
+        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+
+        this._allMonitorItem = new PopupMenu.PopupSwitchMenuItem(_("All monitors"), this._allMonitor)
+        this._allMonitorItem.connect('toggled', this._allMonitorToggle.bind(this));
+        this.menu.addMenuItem( this._allMonitorItem );
+
         this.actor.show();
-    }
-
-    tileWindow() {
-        let windows = this.getWindows();
-
-        let workArea;
-        if (windows.length > 0) {
-            workArea = windows[0].get_meta_window().get_work_area_current_monitor();
-        }
-
-        let width;
-        let height;
-        if (windows.length > 0) {
-            width = Math.round(workArea.width / windows.length)
-        }
-
-        let y = workArea.y;
-        let x = workArea.x;
-        for (let i = 0; i < windows.length; i++) {
-            let win = windows[i].get_meta_window();
-            win.unmaximize(Meta.MaximizeFlags.BOTH);
-            win.move_resize_frame(false, x, y, width, workArea.height);
-            x = x + width;
-        }
     }
 
     cascadeWindow() {
@@ -89,7 +80,9 @@ class ArrangeMenu extends PanelMenu.Button {
 
         let workArea;
         if (windows.length > 0)
-            workArea = windows[0].get_meta_window().get_work_area_current_monitor();
+            workArea = this.getWorkArea(windows[0]);
+        else
+            return;
 
         let y = workArea.y + 5;
         let x = workArea.x + 10;
@@ -98,7 +91,7 @@ class ArrangeMenu extends PanelMenu.Button {
         for (let i = 0; i < windows.length; i++) {
             let win = windows[i].get_meta_window();
             win.unmaximize(Meta.MaximizeFlags.BOTH);
-            win.move_resize_frame(false, x, y, width, height);
+            win.move_resize_frame(true, x, y, width, height);
             x = x + CASCADE_WIDTH;
             y = y + CASCADE_HEIGHT;
         }
@@ -110,8 +103,10 @@ class ArrangeMenu extends PanelMenu.Button {
         let workArea;
         let width;
         if (windows.length > 0) {
-            workArea = windows[0].get_meta_window().get_work_area_current_monitor();
+            workArea = this.getWorkArea(windows[0]);
             width = Math.round(workArea.width / windows.length)
+        } else {
+            return;
         }
 
         let y = workArea.y;
@@ -119,9 +114,43 @@ class ArrangeMenu extends PanelMenu.Button {
         for (let i = 0; i < windows.length; i++) {
             let win = windows[i].get_meta_window();
             win.unmaximize(Meta.MaximizeFlags.BOTH);
-            win.move_resize_frame(false, x, y, width, workArea.height);
-            x = x + width;
+            win.move_resize_frame(true, x, y, width, workArea.height);
+            x = x + win.get_frame_rect().width;
         }
+
+        let last_win = windows[windows.length - 1].meta_window;
+        x = last_win.get_frame_rect().x;
+        last_win.move_resize_frame(false, x, y,
+                                   workArea.x + workArea.width - x,
+                                   workArea.height);
+    }
+
+    stackWindow() {
+        let windows = this.getWindows();
+
+        let workArea;
+        let height;
+        if (windows.length > 0) {
+            workArea = this.getWorkArea(windows[0]);
+            height = Math.round(workArea.height / windows.length)
+        } else {
+            return;
+        }
+
+        let y = workArea.y;
+        let x = workArea.x;
+        for (let i = 0; i < windows.length; i++) {
+            let win = windows[i].get_meta_window();
+            win.unmaximize(Meta.MaximizeFlags.BOTH);
+            win.move_resize_frame(true, x, y, workArea.width, height);
+            y = y + win.get_frame_rect().height;
+        }
+
+        let last_win = windows[windows.length - 1].meta_window;
+        y = last_win.get_frame_rect().y;
+        last_win.move_resize_frame(false, x, y,
+                                   workArea.width,
+                                   workArea.y + workArea.height - y);
     }
 
     maximizeWindow(direction) {
@@ -154,7 +183,24 @@ class ArrangeMenu extends PanelMenu.Button {
             return false;
         });
 
+        if (!(this._allMonitor)) {
+            windows = windows.filter(w => {
+                return w.meta_window.get_monitor() == global.display.get_current_monitor();
+            });
+        }
         return windows;
+    }
+
+    getWorkArea(window) {
+        if (this._allMonitor)
+            return window.get_meta_window().get_work_area_all_monitors();
+        else
+            return window.get_meta_window().get_work_area_current_monitor();
+    }
+
+    _allMonitorToggle() {
+        this._allMonitor = this._allMonitorItem.state;
+        this._gsettings.set_boolean(ALL_MONITOR, this._allMonitorItem.state);
     }
 }
 
